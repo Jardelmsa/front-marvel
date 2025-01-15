427a15420e2f22833d3bf887e145f273f469f4d5
jardel <jardel.morais.prestserv@petrobras.com.br>
2025-01-15 08:51:47 -0300

    configuração de rotas

--- marvel-app/src/app/app.routes.ts
+++ marvel-app/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { CharacterListComponent } from './components/character-list/character-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        redirectTo: 'characters',
+        pathMatch: 'full'
+      },
+      {
+        path: 'characters',
+        component: CharacterListComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'characters'
+      }
+];
